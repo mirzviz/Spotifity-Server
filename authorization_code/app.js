097f86ae678ce81855cb7aaa9be94f3fe3afe023
Zvi Mirimsky <mirzviz@gmail.com>
2019-11-03 17:21:31 +0200

    sg

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -102,7 +102,7 @@ app.get('/callback', function(req, res) {
 
         // we can also pass the token to the browser to make requests from there
         //http://localhost:3000
-        res.redirect('https://spotify-client.herokuapp.com//home#' +
+        res.redirect('https://supify.herokuapp.com/#' +  
           querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
